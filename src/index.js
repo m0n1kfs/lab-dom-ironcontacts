0da0bf0e5378884d2095eb799b09033ea1349118
Monica Fernandez Salinas <monica.fernandez.salinas@gmail.com>
2025-03-26 14:30:18 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-// HTML ELEMENTS
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-
-// ITERATION 0 | Example Row
-// Splice 1 element from the contacts array at the random index
+// -------------------------------
+// ITERATION 0 EXAMPLE (given):
+// -------------------------------
 const randomIndex = Math.floor(Math.random() * contacts.length);
 const splicedArr = contacts.splice(randomIndex, 1);
-
-// Get the element from the spliced array
 const randomContact = splicedArr[0];
 
 const exampleRow = document.createElement("tr");
@@ -27,35 +24,38 @@ exampleRow.innerHTML = `
     </button>
   </td>
 `;
-
 tableBody.appendChild(exampleRow);
 
-
-
-
-
-// ITERATION 1 - Display 3 contacts
-// Get the first 3 contacts from the 'contacts' array.
+// -------------------------------
+// ITERATION 1 | Display 3 Contacts
+// -------------------------------
 const threeContacts = contacts.splice(0, 3);
 
-// Your code goes here ...
+threeContacts.forEach((contact) => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+    <td>
+      <img src="${contact.pictureUrl}" />
+    </td>
+    <td>${contact.name}</td>
+    <td>${contact.popularity.toFixed(2)}</td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+  `;
 
+  // -------------------------------
+  // ITERATION 2 | Delete Buttons
+  // -------------------------------
+  const deleteBtn = newRow.querySelector(".btn-delete");
+  deleteBtn.addEventListener("click", () => {
+    newRow.remove();
+  });
 
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
-
-  // ITERATION 3 - Like Buttons
-
-  // Your code goes here ...
-
-  
-  
-
-
-// Bonus: ITERATION 4 - Add Random Contacts
-
-// Your code goes here ...
+  tableBody.appendChild(newRow);
+});
